ae094d2d0a37d666463487a64a1da54512a67f3f
Aarav D.C <dc.aarav@gmail.com>
2024-07-08 21:17:21 +0545

    Static and Global

---
+++ Recursion/Global.c
@@ -0,0 +1,15 @@
+//The Concept is Same as Static.
+#include<stdio.h>
+int x=0;
+int fun(int n){
+    if(n>0){
+        x++;
+        return fun(n-1)+x;
+    }
+    return 0;
+}
+int main(){
+    int a = 5;
+    printf("%d",fun(a));
+    return 0;
+}
