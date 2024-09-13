b69714feec0d13a32d55896f3947f2bece18ff73
Aarav D.C <dc.aarav@gmail.com>
2024-09-13 20:29:51 +0545

    Insertion

--- Linked List/CLL.cpp
+++ Linked List/CLL.cpp
@@ -14,8 +14,10 @@ class CircularLL{
     CircularLL(int A[],int n);
     void Display();
     void RecDisp();
+    void Insert(int pos,int x);
+    ~CircularLinkedList();
 };
-CircualLL::CircularLL(int A[],int n){
+CircularLL::CircularLL(int A[], int n){
     Node *last,*temp;
     head = new Node;
     head->data = A[0];
@@ -29,7 +31,8 @@ CircualLL::CircularLL(int A[],int n){
         last = temp;
     }
 }
-void CircularLL::Display(){
+void CircularLL::Display()
+{
     Node *p = head;
     do{
         cout<<p->data<<" ";
@@ -45,4 +48,60 @@ void CircularLL::RecDisp(){
         RecDisp(p->next);
     }
     flag = 0;
+}
+void CircularLL::Insert(int pos,int x){
+    Node *p,*t;
+    int i;
+    int length;
+    Node *q =head;
+    do{
+        length++;
+        q=q->next;
+    }while(q!=head);
+
+    if(pos==0){
+        t = new Node;
+        t->data =x;
+        if(head==NULL){
+            head = t;
+            head->next = head;
+        }
+        else{
+            p = head;
+            while(p->next!=head){
+                p=p->next;
+            }
+            p->next=t;
+            t->next = head;
+        }
+    }
+    else{
+        p = head;
+        if(length>pos){
+            cout<<"Invalid Position";
+            return;
+        }
+        for(i=0;i<pos-1;i++){
+            p=p->next;
+        }
+        t =new Node;
+        t->data = x;
+        t->next = p->next;
+        p->next = t;
+    }
+}
+CircularLinkedList::~CircularLinkedList(){
+    Node* p = head;
+    while (p->next != head){
+        p = p->next;
+    }
+    while (p != head){
+        p->next = head->next;
+        delete head;
+        head = p->next;
+    }
+    if (p == head){
+        delete head;
+        head = nullptr;
+    }
 }
