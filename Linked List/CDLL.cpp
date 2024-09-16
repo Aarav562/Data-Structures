7cc37b9a04933e1689bf6ed4fc638e4bbd63ebb0
Aarav D.C <dc.aarav@gmail.com>
2024-09-16 18:54:36 +0545

    Circular Doubly Linked List

--- Linked List/CDLL.cpp
+++ Linked List/CDLL.cpp
@@ -18,7 +18,21 @@ class CDLL{
     ~CDLL();
 }
 CDLL::CDLL(int A[],int n){
-
+    Node *t, *last;
+    head = new Node;
+    head->data = A[0];
+    head->prev = head;
+    head->next = head;
+    last = head;
+    for(int i = 1; i < n; i++){
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next = t;
+        head->prev = t;
+        last = t;
+    }
 }
 void CDLL::Display(){
     Node *p=head;
@@ -56,8 +70,37 @@ void CDLL::Insert(int pos,int x){
     }
 }
 void CDLL::Delete(int pos){
-    
+    if(head == NULL || pos < 1) return;
+    Node *p = head;
+    if(pos == 1){ 
+        if(head->next == head){ 
+            delete head;
+            head = NULL;
+        }
+        else{
+            head->prev->next = head->next;
+            head->next->prev = head->prev;
+            p = head;
+            head = head->next;
+            delete p;
+        }
+    }
+    else{
+        for(int i = 0; i < pos - 1; i++){
+            p = p->next;
+        }
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+        delete p;
+    }
 }
 CDLL::~CDLL(){
-
+   if(head == NULL) return;
+    Node *p = head;
+    do{
+        Node *temp = p;
+        p = p->next;
+        delete temp;
+    }while(p != head);
+    head = NULL;
 }
