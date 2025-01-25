6a0703ff71b76252a9f08ce5ff13c6596cb82b3c
Aarav D.C <dc.aarav@gmail.com>
2025-01-26 00:13:45 +0545

    Djikstras Algorithm

--- Graphs/Graph_List.cpp
+++ Graphs/Graph_List.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Graph{
     int V;
-    list<pair<int,int>> adj;
+    list<pair<int,int>> *adj;
 
     public:
     Graph(int V);
@@ -46,4 +46,25 @@ void Graph::shortest_path(int s){
     for(int i=0;i<V;i++){
         cout<<i<<"\t\t"<<dist[i]<<endl;
     }
+}
+int main(){
+    int V = 9;
+    Graph g(V);
+    g.add_edge(0, 1, 4);
+    g.add_edge(0, 7, 8);
+    g.add_edge(1, 2, 8);
+    g.add_edge(1, 7, 11);
+    g.add_edge(2, 3, 7);
+    g.add_edge(2, 8, 2);
+    g.add_edge(2, 5, 4);
+    g.add_edge(3, 4, 9);
+    g.add_edge(3, 5, 14);
+    g.add_edge(4, 5, 10);
+    g.add_edge(5, 6, 2);
+    g.add_edge(6, 7, 1);
+    g.add_edge(6, 8, 6);
+    g.add_edge(7, 8, 7);
+
+    g.shortest_path(0);
+    return 1;
 }
