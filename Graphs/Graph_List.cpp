9570dcc27da0520404fcc0893a1170bde7a23eda
Aarav D.C <dc.aarav@gmail.com>
2025-01-25 22:05:56 +0545

    Functions To Add Edges

--- Graphs/Graph_List.cpp
+++ Graphs/Graph_List.cpp
@@ -1,2 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Graph{
+    int V;
+    list<pair<int,int>> adj;
+
+    public:
+    Graph(int V);
+
+    void add_edge(int u,int v,int w);
+    void shortest_path(int s);
+};
+
+Graph::Graph(int V){
+    this->V = V;
+    adj = new list<pair<int,int>>[V];
+}
+void Graph::add_edge(int u,int v,int w){
+    adj[u].push_back(make_pair(v,w));
+    adj[v].push_back(make_pair(u,w));
+}
