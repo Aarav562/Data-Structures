38d114c85199edbdec316017e394197f594241a5
Aarav D.C <dc.aarav@gmail.com>
2025-01-28 19:35:04 +0545

    Dijkstra's Algorithm For Adjacency Matrix

--- Graphs/Djikstra_Matrix.cpp
+++ Graphs/Djikstra_Matrix.cpp
@@ -21,7 +21,7 @@ void printSolution(int dist[])
 void Dijkstra(int graph[V][V],int src){
     int dist[V];
     bool setSpt[V];
-    for(int i=0;i<v;i++){
+    for(int i=0;i<V;i++){
         dist[i] = INT_MAX;
         setSpt[i] = false;
     }
@@ -30,10 +30,24 @@ void Dijkstra(int graph[V][V],int src){
         int u = minDist(dist,setSpt);
         setSpt[u] = true;
         for(int v=0;v<V;v++){
-                if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]){
+                if (!setSpt[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]){
                     dist[v] = dist[u] + graph[u][v];
                 }
         }
     }
     printSolution(dist);
+}
+int main(){
+    int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+
+    Dijkstra(graph,0);
+    return 0;
 }
